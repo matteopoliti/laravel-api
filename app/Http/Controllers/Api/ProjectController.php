d91ca95b5dcada83463fbacf47dba860263e8f5e
matteopoliti <matteo.politi17@gmail.com>
2024-04-08 15:37:03 +0200

    Feat: add Show function

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,4 +17,22 @@ class ProjectController extends Controller
             'projects' => $projects,
         ]);
     }
+
+    public function show($slug)
+    {
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project,
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => "Il progetto selezionato non esiste"
+            ]);
+        }
+    }
 }
