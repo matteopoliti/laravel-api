37694d2efeb5dadd9e985d8d219fa6fed70eb7da
matteopoliti <matteo.politi17@gmail.com>
2024-04-11 19:38:11 +0200

    Feat: add lead controller and create Mail

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class LeadController extends Controller
+{
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required | email',
+            'name' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $new_lead = new Lead();
+        $new_lead->fill($data);
+        $new_lead->save();
+
+        Mail::to('info@portfolio.com')->send(new NewContact($new_lead));
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
+}
---
+++ resources/views/emails/new-contact-mail.blade.php
@@ -0,0 +1,8 @@
+<h1>Ciao Admin</h1>
+
+<p>
+    Hai ricevuto un nuovo messaggio, ecco i dettagli: <br>
+    Nome: {{ $lead->name }} <br>
+    Email: {{ $lead->email }} <br>
+    Message: {{ $lead->message }}
+</p>
